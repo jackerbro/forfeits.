0159de6b69658c0b163d08aa6abb047ad7075d13
Evgeni Korotkevich <jacker0809@Gmail.com>
2021-05-20 20:01:31 +0300

    feat: new style

--- src/Components/Content-area/Content.js
+++ src/Components/Content-area/Content.js
@@ -5,8 +5,9 @@ import Button from "./Button/Button";
 
 const arrayName = [
     {name: "Start"},
+    {name: "Record"},
     {name: "Info"},
-    {name: "Exit"}]
+    {name: "Exit"},]
 
 function Content() {
     let buttonList = arrayName.map(btn =>
