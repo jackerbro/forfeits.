e468db16815d2fd9b66fd74fe1ef861bc8b39c8a
Evgeni Korotkevich <jacker0809@Gmail.com>
2021-05-28 14:02:32 +0300

    feat: paje start

--- src/Components/Content-area/Content.js
+++ src/Components/Content-area/Content.js
@@ -6,13 +6,12 @@ function Content() {
 
     return (
         <main className="main">
-            <nav className="list">
-                <Link className="btn" to="/Start">Start</Link>
-                <Link className="btn" to="/Tasks">Add tasks</Link>
-                <Link className="btn" to="/Info">Info</Link>
-                <Link className="btn" to="/Exit">Exit</Link>
-            </nav>
-
+                <nav className="list">
+                    <Link className="btn" to="/Start">Start</Link>
+                    <Link className="btn" to="/Tasks">Add tasks</Link>
+                    <Link className="btn" to="/Info">Info</Link>
+                    <Link className="btn" to="/Exit">Exit</Link>
+                </nav>
         </main>
     )
 }
--- src/Components/Start/Start.js
+++ src/Components/Start/Start.js
@@ -1,8 +1,13 @@
 import React from "react";
+import './Start.scss';
 
 const Start = () => {
     return (<div>
-    <p>ЭТО START</p>
+       <div className="modes">
+           <a className="game-mode" href="">Basic</a>
+           <a className="game-mode" href="">Party</a>
+           <a className="game-mode" href="">...</a>
+       </div>
     </div>)
 }
 
